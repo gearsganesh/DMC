669ae9f02e7a907013292ba8f9ef2b522eff84e1
GaneshGears <128692114+gearsganesh@users.noreply.github.com>
2026-08-27 19:38:11 +0530

    Route /admin to DMC admin CMS

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import Admin from "./Admin";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const isAdminRoute = window.location.pathname.replace(/\/+$/, "") === "/admin";
+root.render(isAdminRoute ? <Admin /> : <App />);
